20ec6fb184e75ac63d3f76e18c6657aaac2c2e34
elisteemus <elisteemus@gmail.com>
2020-08-19 20:41:21 +0300

    login form

--- webshop/src/app/authentication/login/login.component.ts
+++ webshop/src/app/authentication/login/login.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  isSignUp = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onButtonClick() {
+    this.isSignUp = !this.isSignUp;
   }
 
 }
